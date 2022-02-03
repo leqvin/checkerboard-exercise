4a9b4dfdcf662003d1aa8decd286566c52b976ce
leqvin <levin@oregonstate.edu>
2022-02-03 14:00:07 -0800

    red/black done

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Your JS goes here
+
+for (let i= 1; i < 64; i++) {
+    const div = document.createElement('div')
+    div.style.width = '11.1%'
+    div.style.paddingBottom = '11.1%'
+    div.style.float = 'left'
+    if (i % 2 === 1) {
+        div.style.backgroundColor = 'red'; 
+    }
+    else {
+        div.style.backgroundColor = 'black';
+    }
+    
+    document.body.append(div)
+}
+
+
